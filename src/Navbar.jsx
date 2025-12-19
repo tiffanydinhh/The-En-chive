548ef9294a674cf147043c95627c74efb5981fb0
tiffanydinhh <tiffanydinhh@gmail.com>
2025-12-18 21:35:18 -0800

    set up nav bar

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">The En-chive</a>
+        <ul>
+            <li className="active">
+                <a href="/pricing"> Pricing </a>
+            </li>
+            <li>
+                <a href="/about"> About </a>
+            </li>
+        </ul>
+    </nav>
+}
