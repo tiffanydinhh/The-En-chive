1b162029e4c512f54dad5a5d344d397299d6d4d8
tiffanydinhh <tiffanydinhh@gmail.com>
2025-12-19 12:52:16 -0800

    setup navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  let component
-  switch(window.location.pathname) {
-    case "/":
-      component = <App />
-      break
-    case "/pricing":
-      component = <Pricing />
-      break
-    case "/about":
-      component = <About />
-      break
-  }
-  
   return (
     <>
       <Navbar />
-      {component}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,29 @@
+import { useImperativeHandle } from "react"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
 export default function Navbar() {
-    return <nav className="nav">
-        <a href="/" className="site-title">The En-chive</a>
+    return ( 
+        <nav className="nav">
+        <Link to="/" className="site-title">
+            The En-chive
+        </Link>
         <ul>
-            <li className="active">
-                <a href="/pricing"> Pricing </a>
-            </li>
-            <li>
-                <a href="/about"> About </a>
-            </li>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
         </ul>
     </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to) //convert relative paths to absolute paths (make full path)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true }) // end: true eliminates partial matching
+    
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
 }
