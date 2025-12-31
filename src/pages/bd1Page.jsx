84a64100b2e8d78d88e4d7b7605a2dfbfb64654a
tiffanydinhh <tiffanydinhh@gmail.com>
2025-12-30 20:56:18 -0800

    bd1 page info

--- src/pages/bd1Page.jsx
+++ src/pages/bd1Page.jsx
@@ -8,7 +8,19 @@ function BorderDayOne() {
             <BackButton />
             <h1 className="bd1-title">Border: Day One</h1>
             <img className="bd1-album-image" src={bd1AlbumImage} alt="Border Day One Album Image"></img>
-            <p className="bd1-info"> release date</p>
+            <p className="bd1-info1"> <b>RELEASE DATE: </b>November 30, 2020</p>
+            <p className="bd1-info2"> <b>ALBUM SUMMARY: </b>
+                Border: Day One is Enhypen's debut album. It tells the story of the seven members crossing a border from their past, 
+                the survival show that formed the group, I-LAND. It represents the transition from trainees to artists, and explores 
+                the themes of uncertainty, connection, and growth.
+            </p>
+            <p className="bd1-info3"> 
+                <b>ACHIEVEMENTS: </b>
+                <ul>
+                    test
+                </ul>
+            </p>
+
         </div>
     )
 }
