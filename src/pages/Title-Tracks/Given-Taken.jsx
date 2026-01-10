b026e510b6fd1be7b57e052d7507a2d74cc743c7
tiffanydinhh <tiffanydinhh@gmail.com>
2026-01-09 21:50:20 -0800

    given-taken page

--- src/pages/Title-Tracks/Given-Taken.jsx
+++ src/pages/Title-Tracks/Given-Taken.jsx
@@ -25,6 +25,74 @@ function GivenTaken() {
                 </div>
             </div>
 
+            <div className="given-taken-lyrics-section">
+                <h2 className="lyrics-title">LYRICS:</h2>
+                <p className="given-taken-lyrics">wake up in day one <br></br>
+                    첫날의 아침 <br></br>
+                    조명이 비춰 <br></br>
+                    태양의 눈빛 <br></br>
+                    그 빛은 날 불태웠지 <br></br>
+                    그 빛은 나의 눈 가렸지 <br></br>
+                    But I go now 꿈을 좇아 <br></br>
+                    Dive into red sun, no lie <br></br>
+                    수많은 stars 수많은 달 <br></br>
+                    질문의 숲속을 헤매던 나 <br></br>
+                    어둠 속의 저 빛을 따라왔어 <br></br>
+                    가는 선 너머의 날 부르는 너 <br></br>
+                    널 부르는 나 <br></br>
+                    운명의 화살 비 속에서 <br></br>
+                    (To) 마주한 내 안의 경계선 <br></br>
+                    (Free) given or taken oh <br></br>
+                    나의 붉은 눈빛 (to you) <br></br>
+                    난 이제 세상을 뒤집어 <br></br>
+                    (To) 하늘에 내 발을 내디뎌 <br></br>
+                    (Me) given or taken oh <br></br>
+                    내 하얀 송곳니 <br></br>
+                    Oh 난 너에게 걸어가지 <br></br>
+                    두 세계를 연결하지 <br></br>
+                    나의 붉은 눈빛 <br></br>
+                    Oh 난 너에게 걸어가지 <br></br>
+                    신세계에 닿을 때까지 <br></br>
+                    내 하얀 송곳니 <br></br>
+                    내 뒤엔 수천 개의 의심 <br></br>
+                    내 뒤엔 수만 개의 불신 <br></br>
+                    But I go now 꿈을 좇아 <br></br>
+                    Drive through the question for life <br></br>
+                    주어짐과 쟁취함 사이 <br></br>
+                    증명의 기로 위 남겨진 나 <br></br>
+                    저 하늘을 우린 기다려왔어 <br></br>
+                    가는 선 너머의 날 부르는 너 <br></br>
+                    널 부르는 나 <br></br>
+                    운명의 화살 비 속에서 <br></br>
+                    (To) 마주한 내 안의 경계선 <br></br>
+                    (Free) given or taken oh <br></br>
+                    나의 붉은 눈빛 (to you) <br></br>
+                    난 이제 세상을 뒤집어 <br></br>
+                    (To) 하늘에 내 발을 내디뎌 <br></br>
+                    (Me) given or taken oh
+                    내 하얀 송곳니 <br></br>
+                    Red blood 저 왕관에 <br></br>
+                    That blood 흐르는 피 <br></br>
+                    Red blood <br></br>
+                    널 향한 내 손끝은 붉은빛에 물들어만 가 <br></br>
+                    But I'm gonna go <br></br>
+                    (To you) 운명의 화살 비 속에서 <br></br>
+                    (To) 마주한 내 안의 경계선 <br></br>
+                    (Free) given or taken oh <br></br>
+                    나의 붉은 눈빛 (to you) <br></br>
+                    난 이제 세상을 뒤집어 <br></br>
+                    (To) 하늘에 내 발을 내디뎌 <br></br>
+                    (Me) given or taken oh <br></br>
+                    내 하얀 송곳니 <br></br>
+                    Oh 난 너에게 걸어가지 <br></br>
+                    두 세계를 연결하지 <br></br>
+                    나의 붉은 눈빛 <br></br>
+                    Oh 난 너에게 걸어가지 <br></br>
+                    신세계에 닿을 때까지 <br></br>
+                    내 하얀 송곳니 <br></br>
+                </p>
+            </div>
+
         </div>
     )
 }
