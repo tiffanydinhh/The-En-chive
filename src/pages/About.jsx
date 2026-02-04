29d1223a22328de55cb1ce12eb92ee070bf02c88
tiffanydinhh <tiffanydinhh@gmail.com>
2026-02-03 16:38:52 -0800

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,15 @@
 import "./About.css"
+import BackButton from "../BackButton"
 
 function About() {
     return(
-        <h1 className="about-title">ABOUT</h1>
+        <div>
+            <BackButton/>
+            <div className="about-title-wrapper">
+                <h1 className="about-title">ABOUT</h1>
+            </div>
+
+        </div>
     )
 }
 
